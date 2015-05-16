788663d8878f6b9abb926d76db1515539f030eb4
Tung NGUYEN <ntt188@gmail.com>
2015-05-16 14:26:44 +0200

    add project springhibernate

---
+++ spring/src/main/java/fr/tungnguyen/spring/RootConfig.java
@@ -0,0 +1,14 @@
+package fr.tungnguyen.spring;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Project's root config class
+ *
+ * @author tungnguyen
+ *
+ */
+@Configuration
+public class RootConfig {
+
+}
---
+++ springhibernate/src/main/java/fr/tungnguyen/springhibernate/config/RootConfig.java
@@ -0,0 +1,14 @@
+package fr.tungnguyen.springhibernate.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Project's root config
+ *
+ * @author tungnguyen
+ *
+ */
+@Configuration
+public class RootConfig {
+
+}
